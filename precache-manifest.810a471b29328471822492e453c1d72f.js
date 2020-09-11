fa10ab1f60807ef2362acd6fd9e16a6ea4e88cbd
vjsravan <60521157+vjsravan@users.noreply.github.com>
2020-09-11 00:04:37 -0400

    Updates

---
+++ precache-manifest.810a471b29328471822492e453c1d72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011bc62c0899e1e1a1fd037f22c0b2a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6026fb5433f7a363a679",
+    "url": "/monsters-rolodex/static/css/main.c8864461.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6026fb5433f7a363a679",
+    "url": "/monsters-rolodex/static/js/main.f1f85e20.chunk.js"
+  },
+  {
+    "revision": "f553da120d846b640e5e",
+    "url": "/monsters-rolodex/static/js/runtime-main.1dfd2aab.js"
+  }
+]);
